9d4a9b88ecaee596bdf67c4f1fadccc8faf6881c
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:47:42 +0530

    init commit 2

--- src/main/java/com/crowdseats/config_service/ConfigServiceApplication.java
+++ src/main/java/com/crowdseats/config_service/ConfigServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsConfigServiceApplication {
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsConfigServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
