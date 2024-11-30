20d6a146958064b3aad5ce6c19cadc9e23ddbb12
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:25:24 +0530

    initial commit

---
+++ src/main/java/com/crowdseats/config_service/CsConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crowdseats.config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crowdseats/config_service/CsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdseats.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
